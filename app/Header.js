b7c30cab67779c9c32552f3e6cc6200b93ed95ed
OmPanchal <oapanchal@gmail.com>
2026-05-12 23:27:53 +0100

    Created Page navigation through the Header possible

--- app/Header.js
+++ app/Header.js
@@ -1,20 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import HeaderLink from "./HeaderLink";
 import { IoCodeSlashSharp, IoHomeOutline } from "react-icons/io5";
 import { PiBird } from "react-icons/pi";
+import { usePathname } from "next/navigation";
+
+const generatePathDetails = (path, icon, label) => {
+  return { path, icon, label };
+};
 
 const Header = () => {
+  const pathName = usePathname();
+
+  const paths = [
+    generatePathDetails("/", <IoHomeOutline />, "HOME"),
+    generatePathDetails("/aboutme", <PiBird />, "ABOUT_ME"),
+    generatePathDetails("/projects", <IoCodeSlashSharp />, "PROJECTS"),
+  ];
+
   return (
-    <div className="absolute top-0 p-8 flex flex-row items-center gap-12 overflow-scroll max-w-full">
-      <HeaderLink selected label="HOME" link="/">
-        <IoHomeOutline />
-      </HeaderLink>
-      <HeaderLink className="" label="ABOUT_ME" link="/aboutme">
-        <PiBird />
-      </HeaderLink>
-      <HeaderLink className="" label="PROJECTS" link="/projects">
-        <IoCodeSlashSharp />
-      </HeaderLink>
+    <div className="absolute top-0 p-8 flex flex-row items-center gap-12 overflow-scroll max-w-full no-scrollbar">
+      {paths.map((element, idx) => {
+        return (
+          <HeaderLink
+            selected={pathName == element.path}
+            key={idx}
+            label={element.label}
+            link={element.path}
+          >
+            {element.icon}
+          </HeaderLink>
+        );
+      })}
     </div>
   );
 };
