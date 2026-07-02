534e3d7ed9154a9d394644dc7f58f52c329806e1
OmPanchal <oapanchal@gmail.com>
2026-07-03 00:11:09 +0100

    Added basic responsiveness

--- app/projects/page.js
+++ app/projects/page.js
@@ -7,12 +7,10 @@ const Projects = () => {
     <div className="flex flex-row flex-1 items-center bg-black h-full font-[Unbound]">
       <ProjectsSidebar />
       <div className="flex flex-row items-center flex-1 h-svh">
-        <div className="flex flex-col h-full xl:w-[60%] w-full bg-amber-300">
+        <div className="flex flex-col h-svh 2xl:w-[70%] w-full bg-amber-300">
           f
         </div>
-        <div className="flex flex-col h-full w-[40%] bg-amber-800 min-w-[40%]">
-          g
-        </div>
+        <div className="flex flex-col h-svh 2xl:w-[30%] bg-amber-800 2xl:min-w-180 2xl:scale-100 scale-0"></div>
       </div>
     </div>
   );
--- components/projects/ProjectsSidebar.js
+++ components/projects/ProjectsSidebar.js
@@ -10,7 +10,7 @@ const ProjectsSidebar = () => {
   // 2xl show sidebar player else show bottom drawer player?
 
   return (
-    <div className="flex flex-col items-start lg:w-[15%] lg:min-w-fit h-svh bg-black border-r border-r-gray-700 p-8 gap-8 ">
+    <div className="flex flex-col items-start lg:w-[15%] w-full absolute lg:min-w-fit h-svh bg-black border-r border-r-gray-700 p-8 gap-8 ">
       <ProjectsLogo />
       <div className="flex flex-col items-center justify-start w-full flex-1">
         <div className="flex flex-col w-full items-center gap-2">
