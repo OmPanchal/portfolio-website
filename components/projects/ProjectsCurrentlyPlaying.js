8a7929e03086086ed762bc24f873609fefac2863
OmPanchal <oapanchal@gmail.com>
2026-07-04 00:25:35 +0100

    Created a currently playing component to help with phone UI

---
+++ components/projects/ProjectsCurrentlyPlaying.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectsCurrentlyPlaying = () => {
+  return (
+    <div className="lg:scale-0 scale-100 absolute bottom-0 w-full p-12 bg-green-500"></div>
+  );
+};
+
+export default ProjectsCurrentlyPlaying;
