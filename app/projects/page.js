c3306e20aea7f5572c36a273666110f9dcc7b087
OmPanchal <oapanchal@gmail.com>
2026-07-04 00:28:20 +0100

    Added project Header with Sidebar toggle features

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,3 +1,5 @@
+import ProjectsCurrentlyPlaying from "@/components/projects/ProjectsCurrentlyPlaying";
+import ProjectsHeader from "@/components/projects/ProjectsHeader";
 import ProjectsSidebar from "@/components/projects/ProjectsSidebar";
 import ProjectSidebar from "@/components/projects/ProjectsSidebar";
 import React from "react";
@@ -7,10 +9,11 @@ const Projects = () => {
     <div className="flex flex-row flex-1 items-center bg-black h-full font-[Unbound]">
       <ProjectsSidebar />
       <div className="flex flex-row items-center flex-1 h-svh">
-        <div className="flex flex-col h-svh 2xl:w-[70%] w-full bg-amber-300">
-          f
+        <div className="flex flex-col h-svh 2xl:w-[60%] w-full bg-black">
+          <ProjectsHeader />
         </div>
-        <div className="flex flex-col h-svh 2xl:w-[30%] bg-amber-800 2xl:min-w-180 2xl:scale-100 scale-0"></div>
+        <div className="flex flex-col h-svh 2xl:w-[40%] bg-amber-800 2xl:min-w-180 2xl:scale-100 scale-0"></div>
+        <ProjectsCurrentlyPlaying />
       </div>
     </div>
   );
--- components/projects/ProjectsHeader.js
+++ components/projects/ProjectsHeader.js
@@ -1,5 +1,28 @@
-const ProjectHeader = () => {
-  return <div>Header</div>;
+"use client";
+import { useGlobals } from "@/context/GeneralContext";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdMenu } from "react-icons/io";
+
+const ProjectsHeader = () => {
+  const { isProjectsSidebarOpen, setIsProjectsSidebarOpen } = useGlobals();
+
+  return (
+    <div className="w-full bg-black lg:p-10 p-4 flex flex-row lg:justify-start items-center justify-between lg:gap">
+      <button
+        className={`cursor-pointer lg:scale-0 scale-100`}
+        onClick={() => {
+          setIsProjectsSidebarOpen(true);
+        }}
+      >
+        <IoMdMenu className="p-3 size-12 border-gray-700 mr-2 border rounded-full" />
+      </button>
+      <input
+        type="text"
+        placeholder="Search Project"
+        className="bg-white/20 lg:p-6 p-4 lg:text-2xl text-sm outline-none rounded-2xl lg:w-[60%] w-full"
+      />
+    </div>
+  );
 };
 
-export default ProjectHeader;
+export default ProjectsHeader;
