3912b30c1cdc7352c59b031a078da87d9bd000ad
OmPanchal <oapanchal@gmail.com>
2026-07-06 00:14:37 +0100

    Added a global projects state which will store all of the projects to display

--- context/GeneralContext.js
+++ context/GeneralContext.js
@@ -1,17 +1,21 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import projectsData from "../components/projects/projects.json";
 
 const GeneralContext = createContext();
 
 export const GeneralProvider = ({ children }) => {
   const [isProjectsSidebarOpen, setIsProjectsSidebarOpen] = useState(false);
   const [isProjectsPlayerOpen, setIsProjectsPlayerOpen] = useState(false);
+  const [projects, setProjects] = useState(projectsData);
 
   const obj = {
     isProjectsSidebarOpen,
     setIsProjectsSidebarOpen,
     isProjectsPlayerOpen,
     setIsProjectsPlayerOpen,
+    projects,
+    setProjects,
   };
   return (
     <GeneralContext.Provider value={obj}>{children}</GeneralContext.Provider>
