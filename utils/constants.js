99825a5e8c3a1c2b288e30c30939f2c2cf8da842
OmPanchal <oapanchal@gmail.com>
2026-07-08 00:40:48 +0100

    Icon constants for players

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,8 @@ import { IoCodeSlashSharp, IoHomeOutline } from "react-icons/io5";
 import { PiBird } from "react-icons/pi";
 import { generatePathDetails } from "./functions";
 import { CiHome, CiMusicNote1, CiUser } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+import { FaLink } from "react-icons/fa6";
 
 export const paths = [
   {
@@ -20,3 +22,8 @@ export const paths = [
     projectsIcon: <CiMusicNote1 className="lg:size-10 size-6" />,
   },
 ];
+
+export const projectsIcons = {
+  github: <FaGithub className="lg:size-12 size-6" />,
+  other: <FaLink className="lg:size-12 size-6" />,
+};
