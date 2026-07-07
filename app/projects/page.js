0225e25124e41d3c137f6cf1d704dc1c9c55a25c
OmPanchal <oapanchal@gmail.com>
2026-07-08 00:41:05 +0100

    minor UI change

--- app/projects/page.js
+++ app/projects/page.js
@@ -6,7 +6,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="flex flex-row flex-1 items-center bg-black h-full font-[Unbound]">
+    <div className="flex flex-row flex-1 items-center bg-black h-full font-[Unbound] select-none">
       <ProjectsSidebar />
       <div className="flex flex-row items-center flex-1 h-svh">
         <div className="flex flex-col h-svh 2xl:w-[60%] w-full bg-black">
