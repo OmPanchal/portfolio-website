3b56866cf87a28406a71dccaf5dd90927cb4a8f1
OmPanchal <oapanchal@gmail.com>
2026-07-13 21:04:29 +0100

    Added a row for albums

--- components/projects/ProjectsSelector.js
+++ components/projects/ProjectsSelector.js
@@ -5,10 +5,20 @@ import ProjectsAuthorList from "./ProjectsAuthorList";
 import { FaPlay } from "react-icons/fa";
 import logo from "./logo/logo-yellow.png";
 import Image from "next/image";
+import projectsData from "./projects.json";
 
 const ProjectsSelector = () => {
-  const { projects, currentProject, setCurrentProject, setQueueIdx, queue } =
-    useGlobals();
+  const {
+    projects,
+    currentProject,
+    albums,
+    setQueueIdx,
+    queue,
+    setIsQueueTabOpen,
+    currentAlbum,
+    queueIdx,
+    setCurrentAlbum,
+  } = useGlobals();
 
   return (
     <div className="max-h-full flex-1 max-w-full flex flex-col items-start justify-start">
@@ -21,10 +31,12 @@ const ProjectsSelector = () => {
                 className="flex flex-col shrink-0 items-start justify-around lg:gap-4 gap-2 lg:w-84 w-32 cursor-pointer group relative"
                 key={idx}
                 onClick={() => {
-                  setQueueIdx(queue.indexOf(project));
+                  setCurrentAlbum(projectsData.albums[0]);
+                  setQueueIdx(projectsData.projects.indexOf(project));
                 }}
               >
-                {currentProject === project ? (
+                {queue[queueIdx] === project &&
+                currentAlbum.name === "Featured Projects" ? (
                   <div className="absolute top-0 lg:mt-2 mt-1.5 left-0 lg:w-84 w-32 lg:h-84 h-32 flex flex-row items-center justify-center bg-black/30">
                     <Image
                       src={logo}
@@ -52,19 +64,20 @@ const ProjectsSelector = () => {
           })}
         </HorizontalScroll>
       </div>
-      {/* <div className="lg:p-12 p-6 flex w-full flex-col lg:gap-8 gap-4 relative">
+      <div className="lg:p-12 p-6 flex w-full flex-col lg:gap-8 gap-4 relative">
         <p className="lg:text-4xl text-xl font-bold">Alumbs</p>
         <HorizontalScroll className="flex flex-row max-w-full overflow-x-auto lg:gap-8 gap-4 no-scrollbar">
-          {projects.map((project, idx) => {
+          {albums.slice(1, albums.length).map((album, idx) => {
             return (
               <div
                 className="flex flex-col shrink-0 items-start justify-around lg:gap-4 gap-2 lg:w-84 w-32 cursor-pointer group relative"
                 key={idx}
                 onClick={() => {
-                  setQueueIdx(queue.indexOf(project));
+                  setIsQueueTabOpen(true);
+                  setCurrentAlbum(album);
                 }}
               >
-                {currentProject === project ? (
+                {currentAlbum.name === album.name ? (
                   <div className="absolute top-0 left-0 lg:w-84 w-32 lg:h-84 h-32 flex flex-row items-center justify-center bg-black/30">
                     <Image
                       src={logo}
@@ -77,21 +90,18 @@ const ProjectsSelector = () => {
                     <FaPlay className="size-12 text-white/80" />
                   </div>
                 )}
-                <img
-                  src={project?.cover}
-                  className="lg:w-84 w-32 rounded-2xl"
-                />
+                <img src={album?.cover} className="lg:w-84 w-32 rounded-2xl" />
                 <div className="flex flex-col gap-2">
                   <p className="lg:text-2xl text-base text-white font-bold">
-                    {project?.name}
+                    {album?.name}
                   </p>
-                  <ProjectsAuthorList project={project} />
+                  <ProjectsAuthorList project={album} />
                 </div>
               </div>
             );
           })}
         </HorizontalScroll>
-      </div>*/}
+      </div>
     </div>
   );
 };
