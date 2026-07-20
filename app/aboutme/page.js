3091bb04ae31ff940e1b49ae7a3f66a3af73dcfd
OmPanchal <oapanchal@gmail.com>
2026-07-20 22:11:42 +0100

    Updated Pagef

--- app/aboutme/page.js
+++ app/aboutme/page.js
@@ -1,7 +1,12 @@
+import AboutGame from "@/components/about/AboutGame";
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div className="grid place-content-center h-svh w-svw">
+      <AboutGame />
+    </div>
+  );
 };
 
 export default page;
