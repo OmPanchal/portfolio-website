963a0ee799fa2c4d14623b7f051ed1f4282b3334
OmPanchal <oapanchal@gmail.com>
2026-07-22 23:12:15 +0100

    Made the sidebar responsive and threaded state logic

--- components/about/AboutSidebar.js
+++ components/about/AboutSidebar.js
@@ -2,23 +2,51 @@ import { paths } from "@/utils/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import AboutLogo from "./AboutLogo";
+import { useGlobals } from "@/context/GeneralContext";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
-const AboutSidebar = () => {
+const AboutSidebar = ({ state, setState }) => {
   const pathName = usePathname();
+  const {
+    isAboutStatsPageOpen,
+    setIsAboutStatsPageOpen,
+    isAboutSidebarOpen,
+    setIsAboutSidebarOpen,
+  } = useGlobals();
+  const windowSize = useWindowSize();
 
   return (
-    <div className="display flex flex-col items-start justify-center gap-8 text-5xl font-[HyperSpace] fixed h-full left-0 mx-10 font-light">
-      {paths.map((path, idx) => {
-        return (
-          <Link
-            href={path.path}
-            key={idx}
-            className={`${path.path === pathName && "font-bold text-6xl"} hover:font-bold hover:text-6xl`}
-          >
-            {path.name}
-          </Link>
-        );
-      })}
+    <div
+      className={`display flex flex-col items-start justify-center sm:text-5xl text-3xl font-[HyperSpace] fixed h-full ${isAboutSidebarOpen ? "left-0" : "-left-full"} font-light p-4 sm:w-fit w-full sm:z-0 z-30 bg-black`}
+    >
+      <AboutLogo />
+      <div className="flex h-full flex-1 flex-col justify-center gap-8 p-4">
+        {paths.map((path, idx) => {
+          return (
+            <a
+              href={path.path}
+              key={idx}
+              className={`${path.path === pathName && "font-bold sm:text-6xl text-5xl"} hover:font-bold hover:text-6xl`}
+              onClick={() => {
+                setIsAboutSidebarOpen(false);
+              }}
+            >
+              {path.name}
+            </a>
+          );
+        })}
+      </div>
+      {windowSize.width < 640 && (
+        <div
+          className="flex flex-row items-center justify-end w-full"
+          onClick={() => {
+            setIsAboutSidebarOpen(false);
+          }}
+        >
+          {"<"}
+        </div>
+      )}
     </div>
   );
 };
