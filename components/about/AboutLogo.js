cc34cb80d5dd0f8fc4038c9b1d502cd1ba9aad95
OmPanchal <oapanchal@gmail.com>
2026-07-22 23:11:52 +0100

    Made responsive changes to the logo and shifted it into the Sidebar

--- components/about/AboutLogo.js
+++ components/about/AboutLogo.js
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 const AboutLogo = () => {
   return (
-    <div className="flex flex-row items-center text-7xl fixed top-0 left-0 m-6">
+    <div className="flex flex-row items-center sm:text-7xl text-5xl">
       <Image
         src={o}
         alt="o"
-        className=" w-24 mr-2 animate-spin -z-10 [animation-duration:_8s]"
+        className="sm:w-24 w-16 mr-2 animate-spin -z-10 [animation-duration:_8s]"
       />
       <p>m Panchal</p>
     </div>
